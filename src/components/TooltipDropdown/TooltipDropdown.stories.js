a42b71ea819e5b54fb9134653de7a594cf3ac013
Alan Greene <ALANGREE@ie.ibm.com>
2019-12-05 21:17:36 +0000

    wip

--- src/components/TooltipDropdown/TooltipDropdown.stories.js
+++ src/components/TooltipDropdown/TooltipDropdown.stories.js
@@ -14,13 +14,94 @@ limitations under the License.
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
+import { Dropdown } from 'carbon-components-react';
+
 import TooltipDropdown from './TooltipDropdown';
+
+import CheckmarkFilled from '@carbon/icons-react/lib/checkmark--filled/16';
+import CloseFilled from '@carbon/icons-react/lib/close--filled/16';
+import { Spinner } from '@tektoncd/dashboard-components';
 
 const props = {
   id: 'tooltip-dropdown-id',
   label: 'Select an item',
   items: ['item 1', 'item 2', 'item 3'],
   loading: false
+};
+
+const items = [
+  {
+    id: 'option-s1',
+    label: 'skaffold-unit-tests',
+    type: 'TaskRun',
+    status: 'success'
+  },
+  {
+    id: 'option-s2',
+    label: 'run-tests',
+    type: 'step',
+    status: 'success'
+  },
+  {
+    id: 'option-1',
+    label: 'build-skaffold-app',
+    type: 'TaskRun',
+    status: 'error'
+  },
+  {
+    id: 'option-2',
+    label: 'build',
+    type: 'step',
+    status: 'success'
+  },
+  {
+    id: 'option-3',
+    label: 'push',
+    type: 'step',
+    status: 'error'
+  },
+  {
+    id: 'option-4',
+    label: 'build-skaffold-web',
+    type: 'TaskRun'
+  }
+];
+
+const propsFunc = () => ({
+  id: 'carbon-dropdown-example',
+  type: 'default',
+  label: 'Dropdown menu options',
+  ariaLabel: 'Dropdown',
+  disabled: false,
+  light: false,
+  titleText: '',
+  helperText: '',
+  invalid: false,
+  invalidText: 'A valid value is required'
+});
+
+const itemToElement = ({ label, type, status }) => {
+  let icon;
+  switch (status) {
+    case 'error':
+      icon = <CloseFilled style={{ verticalAlign: 'bottom', fill: '#ad1625' }} />;
+      break;
+    case 'success':
+      icon = <CheckmarkFilled style={{ verticalAlign: 'bottom', fill: '#5d8726' }} />;
+      break;
+    default:
+      icon = <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>;
+  }
+  return (
+    <div style={{ verticalAlign: 'middle' }}>
+      {type === 'step' && (
+        <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
+      )}
+      {icon}
+      <span>&nbsp;</span>
+      <span style={{ fontWeight: type === 'TaskRun' ? 'bold' : 'normal' }}>{label}</span>
+    </div>
+  );
 };
 
 storiesOf('TooltipDropdown', module)
@@ -32,4 +113,15 @@ storiesOf('TooltipDropdown', module)
   })
   .add('empty', () => {
     return <TooltipDropdown {...props} items={[]} />;
+  })
+  .add('taskruns', () => {
+    return (
+      <Dropdown
+        {...propsFunc()}
+        items={items}
+        itemToString={item => (item ? <span><CloseFilled style={{ verticalAlign: 'bottom', fill: '#ad1625' }} /> build-skaffold-app: {item.label}</span> : '')}
+        itemToElement={itemToElement}
+        onChange={(...args) => console.log({ args })}
+      />
+    );
   });
