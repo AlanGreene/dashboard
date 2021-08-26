e142e5d62aa003430f97742f1efd00f528f4b8a0
Alan Greene <github.com@alangreene.net>
2021-08-26 19:32:16 +0100

    wip

--- packages/components/src/components/ViewYAML/ViewYAML.stories.js
+++ packages/components/src/components/ViewYAML/ViewYAML.stories.js
@@ -12,8 +12,12 @@ limitations under the License.
 */
 
 import React from 'react';
+import JSONView from 'react-json-view';
 
 import ViewYAML from './ViewYAML';
+
+import events from './examples/events.json';
+import pod from './examples/pod.json';
 
 export default {
   args: {
@@ -42,3 +46,80 @@ export default {
 };
 
 export const Base = args => <ViewYAML {...args} />;
+
+export const Foldable = args => (
+  <JSONView
+    collapsed={2}
+    collapseStringsAfterLength={false}
+    // displayArrayKey={false}
+    displayDataTypes={false}
+    displayObjectSize={false}
+    enableClipboard={false}
+    groupArraysAfterLength={100}
+    iconStyle="triangle"
+    indentWidth={4}
+    name={null}
+    quotesOnKeys={false}
+    // src={events}
+    src={pod}
+    // style={{}}
+    // theme="rjv-default"
+
+    // theme="apathy"
+    // theme="ashes"
+    // theme="atelier-dune"
+    // theme="atelier-forest"
+    // theme="atelier-heath"
+    // theme="atelier-lakeside"
+    // theme="atelier-seaside"
+    // theme="bespin"
+    // theme="brewer"
+    // theme="bright"
+    // theme="chalk"
+    // theme="codeschool"
+    // theme="colors"
+    // theme="default"
+    // theme="eighties"
+    // theme="embers"
+    // theme="flat"
+    // theme="google"
+    // theme="grayscale"
+    // theme="greenscreen"
+    // theme="harmonic"
+    // theme="hopscotch"
+    // theme="isotope"
+    // theme="marrakesh"
+    // theme="mocha"
+    // theme="monokai"
+    // theme="ocean"
+    // theme="paraiso"
+    // theme="pop"
+    // theme="railscasts"
+    // theme="shapeshifter"
+    // theme="solarized"
+    // theme="summerfruit"
+    // theme="threezerotwofour"
+    // theme="tomorrow"
+    // theme="tube"
+    // theme="twilight"
+
+    // theme={{
+    //   base00: "white",
+    //   base01: "#ddd",
+    //   base02: "#ddd",
+    //   base03: "#444",
+    //   base04: "purple",
+    //   base05: "#444",
+    //   base06: "#444",
+    //   base07: "#444",
+    //   base08: "#444",
+    //   base09: "rgba(70, 70, 230, 1)",
+    //   base0A: "rgba(70, 70, 230, 1)",
+    //   base0B: "rgba(70, 70, 230, 1)",
+    //   base0C: "rgba(70, 70, 230, 1)",
+    //   base0D: "rgba(70, 70, 230, 1)",
+    //   base0E: "rgba(70, 70, 230, 1)",
+    //   base0F: "rgba(70, 70, 230, 1)"
+    // }}
+  />
+);
