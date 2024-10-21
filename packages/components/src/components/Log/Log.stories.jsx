61913e6801e68011ef69dcc221035e5ec75cb2e6
Alan Greene <github.com@alangreene.net>
2024-10-21 11:25:55 +0100

    wip: pad values to simulate grid

--- packages/components/src/components/Log/Log.stories.jsx
+++ packages/components/src/components/Log/Log.stories.jsx
@@ -18,8 +18,8 @@ const ansiLog =
   '\n=== demo-pipeline-run-1-build-skaffold-app-2mrdg-pod-59e217: build-step-git-source-skaffold-git-ml8j4 ===\n{"level":"info","ts":1553865693.943092,"logger":"fallback-logger","caller":"git-init/main.go:100","msg":"Successfully cloned https://github.com/GoogleContainerTools/skaffold @ \\"master\\" in path \\"/workspace\\""}\n\n=== demo-pipeline-run-1-build-skaffold-app-2mrdg-pod-59e217: build-step-build-and-push ===\n\u001b[36mINFO\u001b[0m[0000] Downloading base image golang:1.10.1-alpine3.7\n2019/03/29 13:21:34 No matching credentials were found, falling back on anonymous\n\u001b[36mINFO\u001b[0m[0001] Executing 0 build triggers\n\u001b[36mINFO\u001b[0m[0001] Unpacking rootfs as cmd RUN go build -o /app . requires it.\n\u001b[36mINFO\u001b[0m[0010] Taking snapshot of full filesystem...\n\u001b[36mINFO\u001b[0m[0015] Using files from context: [/workspace/examples/microservices/leeroy-app/app.go]\n\u001b[36mINFO\u001b[0m[0015] COPY app.go .\n\u001b[36mINFO\u001b[0m[0015] Taking snapshot of files...\n\u001b[36mINFO\u001b[0m[0015] RUN go build -o /app .\n\u001b[36mINFO\u001b[0m[0015] cmd: /bin/sh\n\u001b[36mINFO\u001b[0m[0015] args: [-c go build -o /app .]\n\u001b[36mINFO\u001b[0m[0016] Taking snapshot of full filesystem...\n\u001b[36mINFO\u001b[0m[0036] CMD ["./app"]\n\u001b[36mINFO\u001b[0m[0036] COPY --from=builder /app .\n\u001b[36mINFO\u001b[0m[0036] Taking snapshot of files...\nerror pushing image: failed to push to destination gcr.io/christiewilson-catfactory/leeroy-app:latest: Get https://gcr.io/v2/token?scope=repository%3Achristiewilson-catfactory%2Fleeroy-app%3Apush%2Cpull\u0026scope=repository%3Alibrary%2Falpine%3Apull\u0026service=gcr.io exit status 1\n\n=== demo-pipeline-run-1-build-skaffold-app-2mrdg-pod-59e217: nop ===\nBuild successful\n\r\r\n';
 
 const levels = ['ERROR', 'WARN ', 'INFO ', 'DEBUG', 'TRACE'];
-const sources = ['script:12', 'file.ts:39'];
-const commands = ['source', 'clone', 'load_artifact', 'collect_evidence', 'test'];
+const sources = ['script:12 ', 'file.ts:39'];
+const commands = ['source          ', 'clone           ', 'load_artifact   ', 'collect_evidence', 'test            '];
 
 const long = Array.from({ length: 60000 }, (v, i) => JSON.stringify({
   command: commands[Math.floor(Math.random() * commands.length)],
@@ -46,7 +46,7 @@ export default {
   component: Log,
   decorators: [
     Story => (
-      <div style={{ width: '500px' }}>
+      <div style={{ width: '1000px' }}>
         <Story />
       </div>
     )
@@ -100,8 +100,10 @@ export const ANSICodes = {
 
 export const Windowed = {
   args: {
+    delimiter: ' ',
     fetchLogs: () => long,
     fields: ['timestamp'],
+    grid: false,
     stepStatus: { terminated: { reason: 'Completed', exitCode: 0 } }
   },
   argTypes: {
@@ -121,8 +123,13 @@ export const Windowed = {
 
           const lineObj = JSON.parse(line);
           const fields = Array.from(new Set(args.fields).add('message'));
-          const output = fields.map(field => lineObj[field] || '').filter(Boolean);
-          return output.join(' ');
+          const output = fields
+            .map(field => {
+              const content = lineObj[field] || '';
+              return args.grid ? content : content.trim();
+            })
+            .filter(Boolean);
+          return output.join(args.delimiter);
         }}
       />
     );
@@ -132,8 +139,10 @@ export const Windowed = {
 
 export const Performance = {
   args: {
+    delimiter: ' ',
     fetchLogs: () => performanceTest,
     fields: ['timestamp'],
+    grid: false,
     stepStatus: { terminated: { reason: 'Completed', exitCode: 0 } }
   },
   argTypes: {
@@ -154,8 +163,13 @@ export const Performance = {
 
           const lineObj = JSON.parse(line);
           const fields = Array.from(new Set(args.fields).add('message'));
-          const output = fields.map(field => lineObj[field] || '').filter(Boolean);
-          return output.join(' ');
+          const output = fields
+            .map(field => {
+              const content = lineObj[field] || '';
+              return args.grid ? content : content.trim();
+            })
+            .filter(Boolean);
+          return output.join(args.delimiter);
         }}
       />
     );
